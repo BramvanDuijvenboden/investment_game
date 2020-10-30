bdb6c9342bdc7c7e5dd79a094753f6702a55b703
van Duijvenboden <bvanduijvenboden@deloitte.nl>
2020-10-30 13:37:44 +0100

    exchange class

---
+++ exchange_bram.py
@@ -0,0 +1,15 @@
+class Exchange:
+    def __init__(self):
+        self.name = "KOBEX"
+        print("Welcome at %s" %self.name)
+
+    def available_stocks(self):
+        stocks = {'ABN AMRO Group':'ABN', 'Ahold Delhaize':'AD', 'Adyen':'ADYEN', 'Aegon':'AGN', 'AkzoNobel':'AKZA',
+                  'ASM International':'ASM', 'ASML Holding':'ASML', 'ASR Nederland':'ASRNL', 'DSM':'DSM',
+                  'Galapagos':'GLPG', 'Heineken':'HEIA', 'IMCD':'IMCD', 'ING Group':'INGA', 'KPN':'KPN',
+                  'ArcelorMittal':'MT',	'NN Group':'NN', 'Philips':'PHIA', 'Prosus':'PRX', 'Randstad':'RAND',
+                  'Royal Dutch Shell':'RDSA', 'RELX':'REN', 'Just Eat Takeaway':'TKWY', 'Unilever':'UNA',
+                  'Unibail-Rodamco-Westfield':'URW', 'Wolters Kluwer':'WKL'}
+        print(stocks.keys())
+
+    def stock_price(stock):
